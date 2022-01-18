185dbdac07e6b0485e481aab711490c89c9d9517
Alexay Polyashov <polyashov@yandex.ru>
2022-01-18 20:55:47 +0300

    Initial commit

---
+++ src/test/java/com/braindocs/braindocs/BraindocsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.braindocs.braindocs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BraindocsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
