9bec0e6e984176d6f906fff906c2db15ea4fb9fe
Alexay Polyashov <polyashov@yandex.ru>
2022-02-17 22:58:11 +0300

    правки в properties

--- src/main/java/com/braindocs/BraindocsApplication.java
+++ src/main/java/com/braindocs/BraindocsApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.MultipartConfigFactory;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.util.unit.DataSize;
 import org.springframework.util.unit.DataUnit;
 
