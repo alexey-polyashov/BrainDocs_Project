20a138e6566e1688799ef01192fa6339a5fcd24e
Alexay Polyashov <polyashov@yandex.ru>
2022-03-04 00:13:13 +0300

    В методы получения списка внесены изменения: - исключены элементы помеченные на удаление - добавлены параметры для отображения помеченных на удаление элементов - marked (значения off, on, only)

--- src/main/resources/db/migration/V22030323__marked.sql
+++ src/main/resources/db/migration/V22030323__marked.sql
@@ -1,12 +1,12 @@
---UPDATE documents SET marked = false WHERE marked is null;
---UPDATE documents_types SET marked = false WHERE marked is null;
---UPDATE organisations SET marked = false WHERE marked is null;
---
---ALTER TABLE documents
---  ALTER COLUMN marked boolean NOT NULL  DEFAULT false;
---
---ALTER TABLE documents_types
---  ALTER COLUMN marked boolean NOT NULL  DEFAULT false;
---
---ALTER TABLE organisations
---  ALTER COLUMN marked boolean NOT NULL  DEFAULT false;
+UPDATE documents SET marked = false WHERE marked is null;
+UPDATE documents_types SET marked = false WHERE marked is null;
+UPDATE organisations SET marked = false WHERE marked is null;
+
+ALTER TABLE documents
+  ALTER COLUMN marked SET NOT NULL;
+
+ALTER TABLE documents_types
+  ALTER COLUMN marked SET NOT NULL;
+
+ALTER TABLE organisations
+    ALTER COLUMN marked SET NOT NULL;
