55fecf04fcb75279b04691f801211007f654bbe7
nick <d.nikkit2012@gmail.com>
2022-03-19 20:59:10 +0300

    add history test data

---
+++ src/main/resources/db/migration/V22031905__history_test_data.sql
@@ -0,0 +1,8 @@
+INSERT INTO operations_history (change_type, author_id)
+VALUES ('Document', 1),
+       ('Document', 1),
+       ('DocumentType', 2),
+       ('Organisation', 2),
+       ('Contact', 2),
+       ('User', 3),
+       ('DocumentType', 3);
